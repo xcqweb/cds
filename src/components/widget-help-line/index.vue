578ced16db7c3ff50fa33166428e47ec2f27c45c
shihong.li <shihong.li@getech.cn>
2021-01-15 19:10:53 +0800

    优化

--- src/components/widget-help-line/index.vue
+++ src/components/widget-help-line/index.vue
@@ -73,7 +73,7 @@ export default {
       }
     },
     selectWidgetsCount() {
-      return this.selectWidgets.length
+      return this.$store.getters.selectWidgets.length
     }
   },
   watch: {
